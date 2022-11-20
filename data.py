0ae71b6bc3677f804a61f4d074408edf342af675
Huy Tran Tuan <trantuanhuyltv@gmail.com>
2022-11-21 05:51:36 +0700

    Change format src

---
+++ data.py
@@ -0,0 +1,191 @@
+homePage = {
+    'vn': {
+        'url': "/vn",
+        'current_title': "VN home page",
+        'template': "./vn/home-page.html",
+        'content': {}
+    },
+    'spain': {
+        'url': "/spain",
+        'current_title': "Spain home page",
+        'template': "./spain/home-page.html",
+        'content': {}
+    }
+}
+aboutPage = {
+    'vn': {
+        'url': "/vn/about-page",
+        'current_title': "VN about page",
+        'template': "./vn/about-page.html",
+        'content': {}
+    },
+    'spain': {
+        'url': "/spain/about-page",
+        'current_title': "Spain about page",
+        'template': "./spain/about-page.html",
+        'content': {}
+    }
+}
+contactPage = {
+    'vn' : {
+        'url': "/vn/contact-page",
+        'current_title': "VN contact page",
+        'template': "./vn/contact-page.html",
+        'content': {}
+    },
+    'spain': {
+        'url': "/spain/contact-page",
+        'current_title': "Spain contact page",
+        'template': "./spain/contact-page.html",
+        'content': {}
+    }
+}
+researchPage = {
+    'vn': {
+        'url': "/vn/research-page",
+        'current_title': "VN research page",
+        'template': "./vn/research-page.html",
+        'content': {}
+    },
+    'spain' : {
+        'url': "/spain/research-page",
+        'current_title': "Spain research page",
+        'template': "./spain/research-page.html",
+        'content': {}
+    }
+}
+disclaimerPage = {
+    'vn': {
+        'url': "/vn/disclaimer-page",
+        'current_title': "VN disclaimer page",
+        'template': "./vn/disclaimer-page.html",
+        'content': {}
+    },
+    'spain': {
+        'url': "/spain/disclaimer-page",
+        'current_title': "Spain disclaimer page",
+        'template': "./spain/disclaimer-page.html",
+        'content': {}
+    }
+}
+
+
+topic = {
+    'vn': [
+        {
+            'name': 'name1',
+            'img': 'photo/topic1.png',
+            'description': 'This is the short description of topic',
+            'link': '#',
+            'btn': 'Xem them'
+        },
+        {
+            'name': 'name2',
+            'img': 'photo/topic2.png',
+            'description': 'This is the short description of topic',
+            'link': '#',
+            'btn': 'Xem them'
+        },
+        {
+            'name': 'name3',
+            'img': 'photo/topic3.png',
+            'description': '',
+            'link': '#',
+            'btn': 'Xem them'
+        },
+        {
+            'name': 'name4',
+            'img': 'photo/topic4.png',
+            'description': 'This is the short description of topic',
+            'link': '#',
+            'btn': 'Xem them'
+        }
+    ],
+    'spain': [
+        {
+            'name': 'name5',
+            'img': 'photo/topic5.png',
+            'description': 'This is the short description of topic',
+            'link': '#',
+            'btn': 'Watch more'
+        },
+        {
+            'name': 'name6',
+            'img': 'photo/topic6.png',
+            'description': 'This is the short description of topic',
+            'link': '#',
+            'btn': 'Watch more'
+        },
+        {
+            'name': 'name7',
+            'img': 'photo/topic7.png',
+            'description': 'This is the short description of topic',
+            'link': '#',
+            'btn': 'Watch more'
+        },
+        {
+            'name': 'name8',
+            'img': 'photo/topic8.png',
+            'description': 'This is the short description of topic',
+            'link': '#',
+            'btn': 'Watch more'
+        }
+    ]
+}
+
+
+topicPage = {
+    'vn':{
+        'name1': {
+            'url': "/vn/topic/name1",
+            'current_title': "VN name1 topic page",
+            'template': "./vn/topic.html",
+            'content': {}
+        },
+        'name2': {
+            'url': "/vn/topic/name2",
+            'current_title': "VN name2 topic page",
+            'template': "./vn/topic.html",
+            'content': {}
+        },
+        'name3': {
+            'url': "/vn/topic/name3",
+            'current_title': "VN name3 topic page",
+            'template': "./vn/topic.html",
+            'content': {}
+        },
+        'name4': {
+            'url': "/vn/topic/name4",
+            'current_title': "VN name4 topic page",
+            'template': "./vn/topic.html",
+            'content': {}
+        },
+    },
+
+    'spain': {
+        'name5': {
+            'url': "/spain/topic/name5",
+            'current_title': "Spain name5 topic page",
+            'template': "./spain/topic.html",
+            'content': {}
+        },
+        'name6': {
+            'url': "/spain/topic/name6",
+            'current_title': "Spain name6 topic page",
+            'template': "./spain/topic.html",
+            'content': {}
+        },
+        'name7': {
+            'url': "/spain/topic/name7",
+            'current_title': "Spain name7 topic page",
+            'template': "./spain/topic.html",
+            'content': {}
+        },
+        'name8': {
+            'url': "/spain/topic/name8",
+            'current_title': "Spain name8 topic page",
+            'template': "./spain/topic.html",
+            'content': {}
+        },
+    }
+}
--- route.py
+++ route.py
@@ -1,74 +1,111 @@
 from passenger_wsgi import app
-from flask import render_template, request, render_template, redirect, url_for
+from flask import render_template, request, render_template, redirect, url_for, send_from_directory
 from datetime import datetime
 import forms
 
+import data
+
 
 @app.context_processor
-def inject_now():
-    return {'now': datetime.utcnow()}
+def inject_now(): return {'now': datetime.utcnow()}
 
+@app.route('/favicon.ico') 
+def favicon(): return send_from_directory('static/icon', 'favicon.ico', as_attachment=True)
 
-#decorated function
 @app.route('/')
+def redirectIndex(): return redirect(url_for('index'))
+
 @app.route('/landing-page')
-def index():
-    return render_template('landing-page.html', current_title='Landing page')
+def index(): return render_template('landing-page.html', current_title='Landing page')
 
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
 
-# Home page
-############################################################
-@app.route('/vn/home-page')
-def homeVN():
-    return render_template('./vn/home-page.html', current_title='VN Home page')
+@app.route('/<country>', methods=['GET'])
+def redirectHome(country): return redirect(url_for('home', country=country))
+
+@app.route('/<country>/home-page', methods=['GET'])
+def home(country):
+    return render_template(
+        data.homePage[country]['template'],
+        current_title=data.homePage[country]['current_title'],
+        content=data.homePage[country]['content'],
+        topics=data.topic[country],
+        home_link=url_for('home', country=country),
+        about_link=url_for('about', country=country),
+        contact_link=url_for('contact', country=country),
+        research_link=url_for('research', country=country),
+        disclaimer_link=url_for('disclaimer', country=country),
+    )
     
 
-@app.route('/lao/home-page', methods=['GET', 'POST'])
-def homeLao():
-    return render_template('./laos/home-page.html', current_title='Laos home page')
+@app.route('/<country>/about-page', methods=['GET'])
+def about(country):
+    return render_template(
+        data.aboutPage[country]['template'],
+        current_title=data.aboutPage[country]['current_title'],
+        content=data.aboutPage[country]['content'],
+        home_link=url_for('home', country=country),
+        about_link=url_for('about', country=country),
+        contact_link=url_for('contact', country=country),
+        research_link=url_for('research', country=country),
+        disclaimer_link=url_for('disclaimer', country=country),
+    )
 
-@app.route('/cam/home-page', methods=['GET', 'POST'])
-def homeCam():
-    return render_template('./cambodia/home-page.html', current_title='Cambodia home page')
+@app.route('/<country>/contact-page', methods=['GET'])
+def contact(country):
+    return render_template(
+        data.contactPage[country]['template'],
+        current_title=data.contactPage[country]['current_title'],
+        content=data.contactPage[country]['content'],
+        home_link=url_for('home', country=country),
+        about_link=url_for('about', country=country),
+        contact_link=url_for('contact', country=country),
+        research_link=url_for('research', country=country),
+        disclaimer_link=url_for('disclaimer', country=country),
+    )
 
-@app.route('/china/home-page', methods=['GET', 'POST'])
-def homeChina():
-    return render_template('./china/home-page.html', current_title='China home page')
-############################################################
+@app.route('/<country>/research-page', methods=['GET'])
+def research(country):
+    return render_template(
+        data.researchPage[country]['template'],
+        current_title=data.researchPage[country]['current_title'],
+        content=data.researchPage[country]['content'],
+        home_link=url_for('home', country=country),
+        about_link=url_for('about', country=country),
+        contact_link=url_for('contact', country=country),
+        research_link=url_for('research', country=country),
+        disclaimer_link=url_for('disclaimer', country=country),
+    )
+
+@app.route('/<country>/disclaimer-page', methods=['GET'])
+def disclaimer(country):
+    return render_template(
+        data.disclaimerPage[country]['template'],
+        current_title=data.disclaimerPage[country]['current_title'],
+        content=data.disclaimerPage[country]['content'],
+        home_link=url_for('home', country=country),
+        about_link=url_for('about', country=country),
+        contact_link=url_for('contact', country=country),
+        research_link=url_for('research', country=country),
+        disclaimer_link=url_for('disclaimer', country=country),
+    )
 
 
-# About page
-############################################################
-@app.route('/vn/about-page')
-def aboutVN():
-    # return render_template('/vn/about-page.html', current_title='About page')
-    form = forms.AddTaskForm()
-    if form.validate_on_submit():
-        print('Submitted title', form.title.data)
-        return render_template('./vn/about-page.html', form=form, title=form.title.data)
-    return render_template('./vn/about-page.html', current_title='VN about page', form=form)
-
-@app.route('/lao/about-page', methods=['GET', 'POST'])
-def aboutLao():
-    form = forms.AddTaskForm()
-    if form.validate_on_submit():
-        print('Submitted title', form.title.data)
-        return render_template('./laos/about-page.html', form=form, title=form.title.data)
-    return render_template('./laos/about-page.html', current_title='Laos about page', form=form)
-
-@app.route('/cam/about-page', methods=['GET', 'POST'])
-def aboutCam():
-    form = forms.AddTaskForm()
-    if form.validate_on_submit():
-        print('Submitted title', form.title.data)
-        return render_template('./cambodia/about-page.html', form=form, title=form.title.data)
-    return render_template('./cambodia/about-page.html', current_title='Cambodia about page', form=form)
-
-@app.route('/china/about-page', methods=['GET', 'POST'])
-def aboutChina():
-    form = forms.AddTaskForm()
-    if form.validate_on_submit():
-        print('Submitted title', form.title.data)
-        return render_template('./china/about-page.html', form=form, title=form.title.data)
-    return render_template('./china/about-page.html', current_title='China about page', form=form)
-############################################################
+@app.route('/<country>/topic/<topic_name>', methods=['GET'])
+def topic(country, topic_name):
+    print(data.topic[country][0]);
+    print(topic_name)
+    return render_template(
+        data.topicPage[country][topic_name]['template'],
+        current_title=data.topicPage[country][topic_name]['current_title'],
+        content=data.topicPage[country][topic_name]['content'],
+        topics=data.topic[country],
+        home_link=url_for('home', country=country),
+        about_link=url_for('about', country=country),
+        contact_link=url_for('contact', country=country),
+        research_link=url_for('research', country=country),
+        disclaimer_link=url_for('disclaimer', country=country),
+    )
