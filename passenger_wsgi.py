42fc7e29c15e5d4c0d6ccf11f2d208a4ba4bc292
Long Dang <longdangvn88@gmail.com>
2023-02-11 22:44:13 -0500

    Update methods for security reasons

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from dotenv import load_dotenv
 
 from flask import Flask
 
@@ -8,7 +9,11 @@ project_root = os.path.dirname(os.path.realpath('__file__'))
 template_path = os.path.join(project_root, 'app/templates')
 static_path = os.path.join(project_root, 'app/static')
 app = Flask(__name__, template_folder=template_path, static_folder=static_path)
-app.config['SECRET_KEY'] = 'cpr4llp' #need to update it
+
+# load the environment variables from the .env file
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = SECRET_KEY #need to update it
 
 from route import *
 
